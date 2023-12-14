765249721adb6e4cb0f05d6d44a1074977e89300
KOBITAhx <152068303+KOBITAhx@users.noreply.github.com>
2023-12-13 23:10:39 -0500

    serverhop.lua

--- serverhop.lua
+++ serverhop.lua
@@ -1,17 +1,17 @@
 local TeleportService = game:GetService("TeleportService")
 local Players = game:GetService("Players")
-local StarterGui = game:GetService("StarterGui")
+local HttpService = game:GetService("HttpService")
 
 local placeId = game.PlaceId
 local serversApi = "https://games.roblox.com/v1/games/" .. placeId .. "/servers/Public?sortOrder=Asc&limit=100"
 
 function getLowestPopulatedServer()
     local servers = game:HttpGet(serversApi)
-    local serverData = game:GetService("HttpService"):JSONDecode(servers)
+    local serverData = HttpService:JSONDecode(servers)
 
     if serverData and serverData.data then
         for _, server in pairs(serverData.data) do
-            if server.playing <= 4 then 
+            if server.playing < 3 then
                 return server.id
             end
         end
@@ -20,18 +20,38 @@ function getLowestPopulatedServer()
     return nil
 end
 
-local targetServerId = getLowestPopulatedServer()
+function getNewestServer()
+    local servers = game:HttpGet(serversApi)
+    local serverData = HttpService:JSONDecode(servers)
 
-if targetServerId then
-    StarterGui:SetCore("SendNotification", {
-        Title = "Script Executed",
-        Text = "by KOBITAh",
-        Duration = 2
-    })
+    if serverData and serverData.data then
+        local newestServer = serverData.data[#serverData.data]
+        return newestServer.id
+    end
 
-    wait(3) 
+    return nil
+end
 
-    TeleportService:TeleportToPlaceInstance(placeId, targetServerId, Players.LocalPlayer)
-else
-    print("No suitable servers found.")
+function teleportToServer(serverId)
+    if serverId then
+        TeleportService:TeleportToPlaceInstance(placeId, serverId, Players.LocalPlayer)
+    else
+        print("No suitable servers found.")
+    end
+end
+
+for i = 1, 10 do
+    local targetServerId = getLowestPopulatedServer()
+    
+    if targetServerId then
+        teleportToServer(targetServerId)
+        break
+    else
+        wait(2) 
+    end
+end
+
+if not targetServerId then
+    local newestServerId = getNewestServer()
+    teleportToServer(newestServerId)
 end
